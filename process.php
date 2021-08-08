0469036218845017d59ab9c4f01af88ecb9e622a
doh <eunuk.doh@verticah.com>
2021-08-08 22:46:51 +0900

    universal font change

--- process.php
+++ process.php
@@ -5,7 +5,7 @@ $email = strip_tags($_POST['email']);
 $message = strip_tags($_POST['message']);
 
 // Send Message
-mail( "youremail@domain.com", "Contact Form Submission",
+mail( "admin@verticah.com", "Contact Form Submission",
 "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $url\nMessage: $message\n",
 "From: Forms <forms@example.net>" );
 ?>
